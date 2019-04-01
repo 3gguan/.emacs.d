edc9665705d2609068cb8e48f30a757bb8b91adf
guan <guan@guanMac.local>
2019-04-02 00:03:33 +0800

    On branch master  Your branch is up to date with 'origin/master'.  modify color

--- elpa/color-theme-molokai-0.1/color-theme-molokai.el
+++ elpa/color-theme-molokai-0.1/color-theme-molokai.el
@@ -20,7 +20,7 @@
   (color-theme-install
    '(color-theme-molokai
      ((foreground-color . "#F8F8F2")
-      (background-color . "#1B1D1E")
+      (background-color . "#434343")
       (cursor-color . "#F8F8F0")
       (background-mode . dark))
      (default ((t (:foreground "#F8F8F2" :background "#1B1D1E"))))
@@ -51,8 +51,8 @@
      (mode-line-mousable ((t (:foreground "#BCBCBC" :background "#000000"))))
      (mode-line-mousable-minor-mode ((t (:foreground "#BCBCBC" :background "#000000"))))
      (font-lock-builtin-face ((t (:foreground "#A6E22E"))))
-     (font-lock-comment-face ((t (:foreground "#465457" :slant italic))))
-     (font-lock-comment-delimiter-face ((t (:foreground "#465457" :slant italic))))
+     (font-lock-comment-face ((t (:foreground "#b0a49b" :slant italic))))
+     (font-lock-comment-delimiter-face ((t (:foreground "#b0a49b" :slant italic))))
      (font-lock-constant-face ((t (:foreground "#AE81FF"))))
      (font-lock-doc-face ((t (:foreground "#E6DB74" :slant italic))))
      (font-lock-function-name-face ((t (:foreground "#F92672" :slant italic))))
